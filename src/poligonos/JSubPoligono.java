0acb5809f266ad54031d53fc0488cfae562ccc0a
Luis <Luis@DESKTOP-0KQ9I2J.iptv.microsoft.com>
2018-12-13 21:47:39 -0300

    punto doc

--- src/poligonos/JSubPoligono.java
+++ src/poligonos/JSubPoligono.java
@@ -6,6 +6,8 @@
 package poligonos;
 
 import static poligonos.JAplicacion.listaDePoligonos;
+import static poligonos.JAplicacion.listaDePuntos;
+import static poligonos.JAplicacion.listaDeRectas;
 import static poligonos.JAplicacion.listaDeSubPoligonos;
 
 /**
@@ -23,24 +25,14 @@ public class JSubPoligono extends javax.swing.JFrame {
         if (p==null){
             p = new JSubPoligono();
         }
-        
-        
-        
-        
-        
-        return p;
-        
-        
+        return p;  
     }
     
-    
-    
-    
-    
-    
-    
+
     public JSubPoligono() {
-        initComponents();
+        initComponents(); 
+        
+        
     }
 
     /**
@@ -56,6 +48,7 @@ public class JSubPoligono extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jComboBox1 = new javax.swing.JComboBox<>();
+        jButton3 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextArea2 = new javax.swing.JTextArea();
@@ -76,7 +69,12 @@ public class JSubPoligono extends javax.swing.JFrame {
             }
         });
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jButton3.setText("CARGAR");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -85,23 +83,28 @@ public class JSubPoligono extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 533, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addGap(76, 76, 76))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(17, 17, 17)
+                        .addComponent(jButton2)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Poligono", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 12))); // NOI18N
@@ -137,7 +140,7 @@ public class JSubPoligono extends javax.swing.JFrame {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 283, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel3Layout.setVerticalGroup(
@@ -191,6 +194,15 @@ public class JSubPoligono extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        
+       for(int i = 0; i<listaDeSubPoligonos.getSize();i++){
+           jComboBox1.addItem(listaDeSubPoligonos.getPoligonos().get(i).toString());
+       }
+        
+        
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -229,6 +241,7 @@ public class JSubPoligono extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
--- src/poligonos/Punto.java
+++ src/poligonos/Punto.java
@@ -7,46 +7,93 @@ package poligonos;
  */
 
 /**
- *@author Edgar Araya
- * @author luisbenitez
+ *Punto.java - Metodos y atributos que controlan un punto de un poligono
+ * @author Edgar Araya
+ * @author Luis Benitez
+ * @version 1.0
  */
+
 public class Punto implements IDesplazable {
     
     private byte x;
     private byte y;
+    
+    /**
+     * Constructor que crea un punto con los argumentos ingresados
+     * @param x Numero de la coordenada en el eje x
+     * @param y Numero de la coordenada en el eje y 
+     */
 
     public Punto(byte x, byte y) {
         this.x = x;
         this.y = y;
     }
     
+    /**
+     * Obtiene coordedna del eje x
+     * @return Retorna la coordenada en X del punto 
+     */
+    
     public byte getX() {
         return x;
     }
 
+     /**
+     * Obtiene coordedna del eje y
+     * @return Retorna la coordenada en Y del punto 
+     */
+    
     public byte getY() {
         return y;
     }
+    
+    /**
+     * Desplaza el punto en el eje X
+     * @param delta unidades que será desplazado el punto
+     */
     
     @Override
     public void desplazarEjeX(byte delta){
         this.x =  (byte) (this.x + delta);
     }
     
+     /**
+     * Desplaza el punto en el eje Y
+     * @param delta unidades que será desplazado el punto
+     */
+    
     @Override
     public void desplazarEjeY(byte delta){
         this.y= (byte)(this.y+delta);
     }
     
+     /**
+     * Calcula la distancia euclidiana entre 2 puntos
+     * @param p punto hasta el cual se calcula distancia
+     * @return distancia entre los 2 puntos
+     */
+    
     public float distancia(Punto p){
         return (float) Math.sqrt(Math.pow((p.x-this.x), 2)+Math.pow((p.y-this.y), 2));
     }
+    
+    /**
+     * Muestra atributos objeto tipo punto en String 
+     * @return puntos x e y en String 
+     */
 
     @Override
     public String toString(){
         return "("+this.x+","+this.y+")";
     }
     
+    /**
+     * Compara 2 puntos Dos puntos son iguales si tienen las mismas coordenadas
+     * @param p otro punto a comparar
+     * @return true en caso de que sean iguales y false en caso contrario
+     */
+    
+    @Override
     public boolean equals(Object o){
         
         if (o instanceof Punto){
